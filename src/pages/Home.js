7a4a8a667a595356deb0075b610283f141d01f05
ismaelcoulibaly <ismael.coulibaly99@gmail.com>
2023-12-06 19:23:48 -0500

    Created a page for Items + added images in the Home container

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,37 +4,42 @@ import {Container, Typography, Grid,Card, CardMedia, CardContent} from "@mui/mat
 const Home = () => {
 
     const products = [
-        { id: 1, name: 'Product 1', price: '$10', image: 'product1.jpg' },
-        { id: 2, name: 'Product 2', price: '$15', image: 'product2.jpg' },
-        { id: 3, name: 'Product 3', price: '$20', image: 'product3.jpg' },
-        // Add more products as needed
+        { id: 1, name: 'Timberland 1', price: '$10', image: require('../assets/images/257215_1.jpg') },
+        { id: 2, name: 'Timberland 2', price: '$15', image: require('../assets/images/241170_1.jpg') },
+        { id: 3, name: 'Timberland 3', price: '$20', image: require('../assets/images/197426_1.jpg') },
+        { id: 4, name: 'Timberland 4', price: '$20', image: require('../assets/images/197426_1.jpg') },
+        { id: 5, name: 'Timberland 5', price: '$20', image: require('../assets/images/197426_1.jpg') },
+        { id: 6, name: 'Timberland 6', price: '$20', image: require('../assets/images/197426_1.jpg') },
     ];
+
     return(
-        <Container>
-            <Grid container spacing={3}>
-                {products.map((item) => (
-                    <Grid item xs={12} sm={6} md={4} key={item.id}>
-                        <Card>
-                            <CardMedia
-                                component="img"
-                                alt={item.name}
-                                height="200"
-                                image={item.image}/>
-                            <CardContent>
-                                <Typography variant="h6" component="div">
-                                    {item.name}
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                    Price: {item.price}
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </Grid>
-                ))}
 
-            </Grid>
+            <Container>
+                <Grid container spacing={3}>
+                    {products.map((item) => (
+                        <Grid item xs={12} sm={6} md={4} key={item.id}>
+                            <Card raised={true}  >
+                                <CardMedia
+                                    component="img"
+                                    alt={item.name}
+                                    height="200"
+                                    image={item.image}/>
+                                <CardContent>
+                                    <Typography variant="h6" component="div">
+                                        {item.name}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        Price: {item.price}
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    ))}
 
-        </Container>
+                </Grid>
+
+            </Container>
+
         );
 }
 
